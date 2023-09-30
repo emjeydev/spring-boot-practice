66468b67539b32f3ef27999187f9f77d2f6e6857
emjeydev <emjeydev@gmail.com>
2023-09-30 05:45:35 +0330

    add workbook 2.9

---
+++ Workbooks/Workbook 2.9/workbook-two-point-nine/src/main/java/dev/emjey/workbooktwopointnine/PageController.java
@@ -0,0 +1,17 @@
+package dev.emjey.workbooktwopointnine;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/away")
+    public String away() {
+        return "away";
+    }
+}
---
+++ Workbooks/Workbook 2.9/workbook-two-point-nine/src/main/java/dev/emjey/workbooktwopointnine/WorkbookTwoPointNineApplication.java
@@ -0,0 +1,13 @@
+package dev.emjey.workbooktwopointnine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkbookTwoPointNineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkbookTwoPointNineApplication.class, args);
+	}
+
+}
