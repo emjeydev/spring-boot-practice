076cb2c4d5dc3517d78880553ec6db7f7572421d
emjeydev <emjeydev@gmail.com>
2023-10-02 06:46:09 +0330

    add link to navbar

--- 003 - SBB - Grade Submission/grade-submission/src/main/java/dev/emjey/gradesubmission/Constants.java
+++ 003 - SBB - Grade Submission/grade-submission/src/main/java/dev/emjey/gradesubmission/Constants.java
@@ -1,5 +1,13 @@
 package dev.emjey.gradesubmission;
 
+// This file is made by EmJey
+// Project: GradeSubmission.java
+// FileName: Constants.java
+// Date: 2023/09/11
+// Modified Date: 2023/10/02
+// Email: emjeydev@gmail.com
+// Github: emjeydev
+
 public class Constants {
     public static final int NOT_FOUND = -1000;
 }
