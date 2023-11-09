64464f8cfaf40c4c2e03e01e373654406ca89b43
emjeydev <emjeydev@gmail.com>
2023-11-09 20:10:01 +0330

    add challenge 3

---
+++ 005 - Challenge 3/challenge-three/src/main/java/dev/emjey/challengethree/ChallengeThreeApplication.java
@@ -0,0 +1,13 @@
+package dev.emjey.challengethree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeThreeApplication.class, args);
+	}
+
+}
---
+++ 005 - Challenge 3/challenge-three/src/test/java/dev/emjey/challengethree/ChallengeThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.emjey.challengethree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
