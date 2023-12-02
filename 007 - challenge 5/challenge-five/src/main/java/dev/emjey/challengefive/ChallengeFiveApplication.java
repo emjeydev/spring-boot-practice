faf13f630649edbe265088a03b3d754a59c84ea7
emjeydev <emjeydev@gmail.com>
2023-12-02 22:58:02 +0330

    add challenge 5

---
+++ 007 - challenge 5/challenge-five/src/main/java/dev/emjey/challengefive/ChallengeFiveApplication.java
@@ -0,0 +1,13 @@
+package dev.emjey.challengefive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeFiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeFiveApplication.class, args);
+	}
+
+}
---
+++ 007 - challenge 5/challenge-five/src/test/java/dev/emjey/challengefive/ChallengeFiveApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.emjey.challengefive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeFiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
