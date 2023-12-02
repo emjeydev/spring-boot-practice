7bb4d724ae5d9e335af2e23b25a761a4aab58092
emjeydev <emjeydev@gmail.com>
2023-12-02 22:18:05 +0330

    add workbook 5.1

---
+++ Workbooks/Workbook 5.1/workbook-five-point-one/src/main/java/emjey/dev/workbookfivepointone/WorkbookFivePointOneApplication.java
@@ -0,0 +1,13 @@
+package emjey.dev.workbookfivepointone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkbookFivePointOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkbookFivePointOneApplication.class, args);
+	}
+
+}
---
+++ Workbooks/Workbook 5.1/workbook-five-point-one/src/test/java/emjey/dev/workbookfivepointone/WorkbookFivePointOneApplicationTests.java
@@ -0,0 +1,13 @@
+package emjey.dev.workbookfivepointone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkbookFivePointOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
