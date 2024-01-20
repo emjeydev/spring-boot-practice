11c9183ee8453ab0a3ac183ac38d16c0e0e5d722
emjeydev <emjeydev@gmail.com>
2024-01-20 21:19:27 +0330

    add get-post-delete request to StudentController

--- Workbooks/Workbook 9.1/workbook-nine-point-one/src/main/java/dev/emjey/workbookninepointone/web/StudentController.java
+++ Workbooks/Workbook 9.1/workbook-nine-point-one/src/main/java/dev/emjey/workbookninepointone/web/StudentController.java
@@ -1,7 +1,11 @@
 package dev.emjey.workbookninepointone.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.emjey.workbookninepointone.entity.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 // This file is made by EmJey
 // Project: Workbook 9.1 - GradeSubmission
@@ -14,5 +18,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/student")
 public class StudentController {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable Long id) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Student> saveStudent(@RequestBody Student student) {
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteStudent(@PathVariable Long id) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Student>> getStudents() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
