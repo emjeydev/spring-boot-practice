6a529e08c3ed023f0a9503d8d4a2331fea426afe
EmJeyDev <emjeydev@gmail.com>
2024-04-13 11:19:33 +0330

    add cascade parameter to bidirectional relationship

--- 011 - Grade Submission (Spring JPA)/grade-submission/src/main/java/dev/emjey/gradesubmission/entity/Student.java
+++ 011 - Grade Submission (Spring JPA)/grade-submission/src/main/java/dev/emjey/gradesubmission/entity/Student.java
@@ -39,7 +39,7 @@ public class Student {
     private LocalDate birthDate;
 
     @JsonIgnore
-    @OneToMany(mappedBy = "student")
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
     private List<Grade> grades;
 
 }
