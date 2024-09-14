b8215db09a5555822f0c6f3a7434f5188f8512be
emjeydev <emjeydev@gmail.com>
2024-09-14 21:22:32 +0330

    add springSecurityExample project

---
+++ 014 - Spring Security Example/spring-security-tutorial-eample/src/main/java/dev/emjey/spring_security_tutorial_eample/SpringSecurityTutorialEampleApplication.java
@@ -0,0 +1,13 @@
+package dev.emjey.spring_security_tutorial_eample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTutorialEampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTutorialEampleApplication.class, args);
+	}
+
+}
---
+++ 014 - Spring Security Example/spring-security-tutorial-eample/src/test/java/dev/emjey/spring_security_tutorial_eample/SpringSecurityTutorialEampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.emjey.spring_security_tutorial_eample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTutorialEampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
